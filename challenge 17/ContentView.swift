ff6485b1c27397c4f68b18032143292072f93d1d
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2022-11-18 16:55:06 +0000

    Initial Commit

---
+++ challenge 17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  challenge 17
+//
+//  Created by Luk'yanov, Mikhail (JMOB) on 18/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ challenge 17/challenge_17App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_17App.swift
+//  challenge 17
+//
+//  Created by Luk'yanov, Mikhail (JMOB) on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct challenge_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
